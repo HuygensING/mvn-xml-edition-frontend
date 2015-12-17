3971152929d3b527236797607c399481853c4aa6
renevanderark <r.van.der.ark@gmail.com>
2015-12-17 12:06:47 +0100

    renames search endpoint as per MVN-48

--- js/app/config.js
+++ js/app/config.js
@@ -3,7 +3,6 @@ define(['underscore', 'backbone'], function (_, Backbone) {
 		rootElement: '#app',
 		viewManagerRoot: '#main .view',
 		urlPrefix: '/' + PROJECT_ID + '/',
-		// TODO wordt: http://api.mvn.huygens.knaw.nl/edition/[Project ID]/search (nu)
 		searchURL: 'http://api.mvn.huygens.knaw.nl/editions/' + PROJECT_ID +  '/search',
 		headerURL: 'http://mvn.huygens.knaw.nl/?page_id=65'
 	};
