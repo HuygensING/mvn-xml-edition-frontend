b4ee3fc95f441e357280d5cd8e129150e1d0390d
Gijsjan <agijsbro@gmail.com>
2021-07-09 15:11:17 +0200

    Add server for DO deploy

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,8 @@ document.addEventListener("DOMContentLoaded", async function() {
 	$("#signature-tag").html(dataStructure.get('signatuur'));
 	$("title").html(dataStructure.get('title'));
 	$('header').css({opacity: "1"});
+
+	$('body').prepend(`<div style="color: white; text-transform: uppercase; font-weight: bold; padding: 5px; text-align: center; transform: rotate(30deg); position: fixed; width: 285px; right: -97px; z-index: 9999; top: 18px; letter-spacing: 0.1em; background: #de0f0f; box-shadow: 0 0 12px black;`)
 })
 
 // requirejs.config({
